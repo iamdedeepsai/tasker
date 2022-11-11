ad63389963d8e6621e1f3ea6d599bfba74032d9e
gitxxyl <85171457+gitxxyl@users.noreply.github.com>
2022-11-11 21:39:54 +0800

    adding ktor + heroku

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,30 @@
+package com.example
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.example.plugins.*
+import io.ktor.server.http.content.*
+import io.ktor.server.locations.*
+import io.ktor.server.routing.*
+
+fun main() {
+    embeddedServer(Netty, port = System.getenv("PORT").toInt(), host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureSerialization()
+    configureRouting()
+}
+fun Application.configureRouting() {
+    install(Locations) {
+    }
+    routing {
+        static("/"){
+            staticBasePackage = "files"
+            defaultResource("index.html")
+        }
+    }
+}
